816991b082f02407333c1fc83076cbe4cb9e86c4
Joshua <joshmack02@gmail.com>
2022-12-30 19:21:00 -0800

    User is able to control grid size

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+const newGridBtn = document.querySelector('#create-grid');
 
 
 function createGrid (num) {
@@ -6,10 +7,22 @@ function createGrid (num) {
     for (let i = 0; i < squaredNum; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('sketchbox');
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.setProperty('background-color', 'black');
+        })
         gridContainer.append(gridSquare);
     }
     gridContainer.style.setProperty('grid-template-columns', `repeat(${num}, 1fr)`);
     gridContainer.style.setProperty('grid-template-rows', `repeat(${num}, 1fr)`);
+    
+
 }
 
-createGrid(20);
+// createGrid(20);
+
+newGridBtn.addEventListener('click', () => {
+    gridContainer.innerHTML = '';
+    let userNum = prompt("Enter grid's side length");
+    createGrid(userNum);
+    console.log('clicked');
+});
